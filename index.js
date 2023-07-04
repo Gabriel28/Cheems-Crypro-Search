a191c74f1eead2945d6e54b23c1ff011b91e208f
Gabriel28 <nasc.gabriel.1997@gmail.com>
2023-07-03 21:24:59 -0300

    Add error handler, and refactor fetchs

--- index.js
+++ index.js
@@ -8,47 +8,74 @@ cryptoForm.addEventListener('submit', async (event) => {
     const endpoint = "/api/v3/simple/price?";
     const coins = `ids=${cryptoName}&vs_currencies=usd&include_24hr_change=true`;
 
-    fetch(`${url}${endpoint}${coins}`)
-        .then(res => res.json())
-        .then(json => {
-            const coinData = Object.entries(json);
-            cryptoResult.innerHTML = ""; // Limpar o conteúdo anterior
-
-            coinData.forEach(([coin, coinDetails]) => {
-                const price = coinDetails.usd;
-                const change = coinDetails.usd_24h_change;
-
-                getImage(coin)
-                    .then(imageUrl => {
-                        cryptoResult.innerHTML += `
-                            <div class="coin ${change < 0 ? 'down' : 'up'}">
-                                <div class="coin-logo" mb-3>
-                                    <img src="${imageUrl}"></img>
-                                </div>
-                                <div class="coin-name" mb-3>
-                                    <h3>${coin}</h3>
-                                    <span>/USD</span>
-                                </div>
-                                <div class="coin-price" mb-3>
-                                    <span class="price">$${price.toLocaleString(undefined, { style: 'currency', currency: 'USD' })}</span>
-                                    <span class="change">$${change}</span>
-                                </div>
-                            </div>                    
-                        `;
-                    });
-            });
-        });
+    try {
+        const response = await fetch(`${url}${endpoint}${coins}`);
+        const json = await response.json();
+        const coinEmpty = Object.entries(json).length === 0;
+        getCoinSuccess(json, coinEmpty);
+    } catch (error) {
+        console.log('Ocorreu um erro:', error.message);
+    }
 });
 
-function getImage(crypto) {
+async function getCoinSuccess(json, coinEmpty = false) {
+    cryptoResult.innerHTML = "";
+
+    if (!coinEmpty) {
+        const coinData = Object.entries(json);
+
+        for (const [coin, coinDetails] of coinData) {
+            const price = coinDetails.usd;
+            const change = coinDetails.usd_24h_change;
+
+            try {
+                const imageUrl = await getImage(coin);
+                cryptoResult.innerHTML += `
+                    <div class="coin ${change < 0 ? 'down' : 'up'}">
+                        <div class="coin-logo" mb-3>
+                            <img src="${imageUrl}"></img>
+                        </div>
+                        <div class="coin-name" mb-3>
+                            <h3>${coin}</h3>
+                            <span>/USD</span>
+                        </div>
+                        <div class="coin-price" mb-3>
+                            <span class="price">$${price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</span>
+                            <span class="change">${change}</span>
+                        </div>
+                    </div>
+                `;
+            } catch (error) {
+                console.log('Ocorreu um erro ao obter a imagem:', error.message);
+            }
+        }
+    } else {
+        cryptoResult.innerHTML += `
+            <div class="coin-logo" mb-3>
+                <img src="images/cheems-sad.jog"></img>
+            </div>
+            <div class="coin-name" mb-3>
+                <h3>Aii.. Não encontrei</h3>
+            </div>
+            <div class="coin-price" mb-3>
+                <p>Tente novamente</p>
+            </div>
+        `;
+    }
+}
+
+async function getImage(crypto) {
     const url = "https://api.coingecko.com";
     const endpoint = "/api/v3/coins/";
     const coins = `${crypto}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`;
 
-    return fetch(`${url}${endpoint}${coins}`)
-        .then(res => res.json())
-        .then(json => {
-            const coinDetails = json;
-            return coinDetails.image && coinDetails.image.large;
-        });
+    try {
+        const response = await fetch(`${url}${endpoint}${coins}`);
+        const json = await response.json();
+        const coinDetails = json;
+        return coinDetails.image && coinDetails.image.large;
+    } catch (error) {
+        console.log('Ocorreu um erro ao obter a imagem:', error.message);
+        throw error;
+    }
 }
